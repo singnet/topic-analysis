d61a7742b89eea170f91102455e0b65e3139470b
Eyob Yirdaw <edyirdaw@gmail.com>
2019-02-19 10:23:51 +0300

    exp with docker and supervisor

--- snet_test_client.py
+++ snet_test_client.py
@@ -30,6 +30,20 @@ def sample_data():
 
     return docs
 
+def sample_data_2():
+
+    path = str(pathlib.Path(os.path.abspath('')).parents[0])+'/topic-analysis/docs/tests/test_doc.txt'
+
+    docs = []
+
+    with open(path, "r") as read_file:
+        text = read_file.read().splitlines()
+
+    for k in text:
+        if k != "":
+            docs.append(k)
+
+    return docs
 
 def csv_reader():
 
@@ -58,7 +72,7 @@ def csv_reader():
 
 
 def try_plsa():
-    channel = grpc.insecure_channel('localhost:5000')
+    channel = grpc.insecure_channel('localhost:5001')
     # channel = grpc.insecure_channel('172.17.0.75:5001')
     stub = topic_analysis_pb2_grpc.TopicAnalysisStub(channel)
 
@@ -75,6 +89,22 @@ def try_plsa():
     print(resp.handle)
 
 
+def try_plsa_2():
+    channel = grpc.insecure_channel('localhost:5000')
+    # channel = grpc.insecure_channel('172.17.0.75:5001')
+    stub = topic_analysis_pb2_grpc.TopicAnalysisStub(channel)
+
+
+    plsa_request = topic_analysis_pb2.PLSARequest(docs=sample_data_2(),num_topics=2,maxiter=22,beta=1)
+
+    resp = stub.PLSA(plsa_request)
+
+
+    print(resp.status)
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    print(resp.message)
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    print(resp.handle)
 
 
 
@@ -83,7 +113,8 @@ def try_plsa():
 
 if __name__ == '__main__':
 
-    try_plsa()
+    # try_plsa()
+    try_plsa_2()
     # csv_reader()
 
 
